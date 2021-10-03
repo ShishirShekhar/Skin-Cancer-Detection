977ec58dc4dd08bfe47523e9cb267676dbb7a4e2
Shishir Shekhar <71517975+ShishirShekhar@users.noreply.github.com>
2021-10-03 22:51:44 +0530

    fixing error v2

--- model.py
+++ model.py
@@ -4,5 +4,5 @@ import tensorflow as tf
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model = tf.keras.models.load_model(".\SkinCancer.h5")
+  model = tf.keras.models.load_model("SkinCancer.h5")
   return model
